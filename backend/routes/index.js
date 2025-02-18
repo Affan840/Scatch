745bbc4baed6a2870eba7f3835d69a610dfd1c81
Affan840 <muhammadaffan070000@gmail.com>
2025-02-18 18:17:18 +0500

    1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,24 @@
-import {Router} from 'express';
-import isLoggedIn from '../middlewares/isLoggedIn.middlewares.js';
-import { Product } from '../models/product.model.js';
+import { Router } from "express";
+import isLoggedIn from "../middlewares/isLoggedIn.middlewares.js";
+import { Product } from "../models/product.model.js";
 
 const router = Router();
 
-router.get("/", isLoggedIn,async (req, res) => {
+router.get("/", isLoggedIn, async (req, res) => {
   if (req.user) {
-    res.status(200).json({user: req.user });
+    res.status(200).json({ user: req.user });
   }
 });
 
-
-
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
 
 export default router;
