417c3f480484779ac4ac4b39319604e08e4a59b7
Affan840 <muhammadaffan070000@gmail.com>
2025-02-20 20:06:06 +0500

    Change api endpoint

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,14 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -27,11 +35,14 @@ import authRoute from "./routes/index.js";
 import ordersRoute from "./routes/orders.routes.js";
 import { Product } from "./models/product.model.js";
 
-app.use("/shop",authRoute);
+app.use("/",authRoute);
 app.use("/owners", ownersRoute);
 app.use("/users", usersRoute);
 app.use("/products", productsRoute);
 app.use("/orders", ordersRoute);
+
+
+
 
 connectDB()
   .then(() => {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,15 +5,6 @@ import { Product } from "../models/product.model.js";
 const router = Router();
 
 router.get("/", isLoggedIn, async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
-
   if (req.user) {
     return res.status(200).json({ user: req.user });
   } else {
