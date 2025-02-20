abe0d494a3b432aebd35a37fe7f8c9d787f8c610
Affan840 <muhammadaffan070000@gmail.com>
2025-02-20 11:32:55 +0500

    change cors origin

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,18 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN?.split(","), 
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
