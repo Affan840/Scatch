378b4336d690c9bf80cf4f5a1aa2b5a398c40926
Affan840 <muhammadaffan070000@gmail.com>
2025-02-24 18:22:41 +0500

    change in products styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 
   return (
     <div className="flex h-screen relative xl:px-32 2xl:px-60 px-20 flex-col items-center justify-center lg:flex-row">
-      <div className="lg:hidden flex justify-between absolute top-40 gap-4 mb-4 py-1 rounded-md bg-zinc-200">
+      <div className="lg:hidden flex justify-between absolute top-36 gap-4 mb-4 py-1 rounded-md bg-zinc-200">
         <button
           className={`px-4 py-2 rounded-md ${activeTab === "signup" ? "bg-blue-400 text-white" : "bg-zinc-200 text-black"}`}
           onClick={() => setActiveTab("signup")}
