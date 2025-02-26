998b2ef57bbd714a3274bfe1e4d3b73fb2ecb117
Affan840 <muhammadaffan070000@gmail.com>
2025-02-26 18:26:59 +0500

    change in nav.jsx

--- backend/middlewares/isLoggedIn.middlewares.js
+++ backend/middlewares/isLoggedIn.middlewares.js
@@ -5,15 +5,16 @@ const isLoggedIn = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
     req.user = null;
+    
     return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
     let decoded = jwt.verify(token, process.env.JWT_SECRET);
     let user = await User.findOne({ email: decoded.email }).select("-password");
-    if (!user) return res.status(401).json({ message: "User not found" });
+    req.user = user;
+    next();
 
-    res.status(200).json({ user });
   } catch (error) {
     res.status(401).json({ message: "Invalid token" });
   }
