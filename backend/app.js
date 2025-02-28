b67c916b4ec822f7ddeb35c0e5e5572a4b462ff1
Affan840 <muhammadaffan070000@gmail.com>
2025-02-28 18:05:37 +0500

    change in nav.jsx

--- backend/app.js
+++ backend/app.js
@@ -13,17 +13,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
- 
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
