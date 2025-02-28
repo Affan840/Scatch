3bae64f705cf156caa7e4d78a3f3a28233876357
Affan840 <muhammadaffan070000@gmail.com>
2025-02-28 16:45:42 +0500

    change in nav.jsx

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -54,17 +54,16 @@ const loginUser = async (req, res) => {
       let token = generateToken(user);
       console.log("cookie set", token);
       res.cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "none",
-        maxAge: 3600000,
+        httpOnly: true, // Prevents JavaScript access
+        secure: process.env.NODE_ENV === "production", // Set true only in production
+        sameSite: "None", // Required for cross-origin cookies
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         path: "/",
       });
-      res.status(200).json(user);
+      res.status(200).json({ message: "Login successful", user });
     });
   } catch (error) {
-    console.log("Server error:", error.message);
-    res.status(500).send("Internal Server Error");
+    res.status(500).json({ message: "Server error", error });
   }
 };
 
