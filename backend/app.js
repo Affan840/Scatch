3eae5e13e8304cdc9accaab9d3aaccf00a2f3a07
Affan840 <muhammadaffan070000@gmail.com>
2025-02-28 18:01:28 +0500

    change in nav.jsx

--- backend/app.js
+++ backend/app.js
@@ -14,20 +14,15 @@ app.use((req, res, next) => {
 });
 
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,  
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+ 
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
